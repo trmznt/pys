6f64d8a6f80cc89820d3a2e2d0dc2886062561cd
Anto - alrescha <anto@eijkman.go.id>
2018-09-17 10:42:41 +0700

    add selection code

--- wgs/txt2select.py
+++ wgs/txt2select.py
@@ -12,7 +12,9 @@ import numpy as np
 
 def init_argparser():
     p = arg_parser("Select CHROM and POS based on some criteria")
-    p.add_argument('--max', type=int, default=100)
+    p.add_argument('--topmax', type=int, default=100)
+    p.add_argument('--topmin', type=int, default=None)
+    p.add_argument('--minthreshold', type=float, default=None)
     p.add_argument('--column', required=True)
     p.add_argument('-o', '--outfile', default="outfile.pos.txt")
 
@@ -28,21 +30,31 @@ def main( args ):
 
 def txt2select( args ):
 
-	df = pandas.read_table(args.infile, delimiter='\t', na_values='  nan')
+    df = pandas.read_table(args.infile, delimiter='\t', na_values='  nan')
 
-	filtered_positions = {}
+    filtered_positions = {}
 
-	for i in args.column.split(','):
+    for i in args.column.split(','):
 
-		i = int(i)
-		df_filtered = df.nlargest(args.max, df.columns[i])
-		for r in df_filtered.itertuples():
-			filtered_positions[(r[1], r[2])] = True
+        i = int(i) - 1
+        column = df.columns[i]
+        cerr('I: selecting with column %s' % column)
 
-	with open(args.outfile, 'w') as outfile:
-		outfile.write('CHROM\tPOS\n')
-		for k in filtered_positions:
-			outfile.write('%s\t%s\n' % (k))
+        if args.minthreshold is not None:
+            df_filtered = df[ df[column] > args.minthreshold ]
+        else:
+            df_filtered = df.nlargest(args.topmax, column)
+        for r in df_filtered.itertuples():
+            filtered_positions[ (r[1], int(r[2])) ] = True
+
+    sorted_positions = sorted( filtered_positions.keys() )
+
+    with open(args.outfile, 'w') as outfile:
+        outfile.write('CHROM\tPOS\n')
+        for k in sorted_positions:
+            outfile.write('%s\t%d\n' % (k))
+
+    cerr('I: writing %d positions' % len(sorted_positions))
 
 
 
