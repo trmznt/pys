ea12a4b859ef37077fa50e13b80cc20e0dd4d979
Hidayat Trimarsanto <anto@eijkman.go.id>
2018-09-19 08:15:57 +0700

    change tabs to spaces

--- wgs/geno2genediv.py
+++ wgs/geno2genediv.py
@@ -44,24 +44,24 @@ def geno2genediv( args ):
     				'\t'.join( group_keys ))
 
     for idx, region in enumerate(lineparser.parse_genes()):
-    	haplotypes = set( region.haplotypes())
-    	enc_haplos = region.encode_haplotypes()
-    	assert len(haplotypes) == max(enc_haplos) + 1
-    	haploarray = allel.HaplotypeArray( [enc_haplos] )
+        haplotypes = set( region.haplotypes())
+        enc_haplos = region.encode_haplotypes()
+        assert len(haplotypes) == max(enc_haplos) + 1
+        haploarray = allel.HaplotypeArray( [enc_haplos] )
 
-    	cerr( 'I: calculating %d - %s' % (idx, region.name))
+        cerr( 'I: calculating %d - %s' % (idx, region.name))
 
-    	value = []
-    	for g in group_keys:
+        value = []
+        for g in group_keys:
             ac_g = haploarray.count_alleles(subpop = groups[g])
             ac_ng = haploarray.count_alleles(subpop = list( lineparser.sample_idx - set(groups[g])))
             num, den = allel.stats.hudson_fst(ac_g, ac_ng)
             value.append(den)
 
-    	#print(idx, '%4d' % len(haplotypes), max(enc_haplos), region.name, value)
-    	params = ( np.mean(value), np.median(value), np.max(value), np.min(value))
-    	outfile.write('%s\t%s\t%s\t%d\t%d\t%s\t%s\n' % (
-    			region.P[0][0], region.P[0][1], region.name, len(region.P), len(haplotypes),
-    			'\t'.join( '%5.4f' % x for x in params),
+        #print(idx, '%4d' % len(haplotypes), max(enc_haplos), region.name, value)
+        params = ( np.mean(value), np.median(value), np.max(value), np.min(value))
+        outfile.write('%s\t%s\t%s\t%d\t%d\t%s\t%s\n' % (
+                region.P[0][0], region.P[0][1], region.name, len(region.P), len(haplotypes),
+                '\t'.join( '%5.4f' % x for x in params),
     			'\t'.join( '%5.4f' % x for x in value)))
 
