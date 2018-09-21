78b7e21acb3781ea93d5352cc4538a9f616b7b41
Anto - alrescha <anto@eijkman.go.id>
2018-09-21 07:37:04 +0700

    complete geno2prune code

--- wgs/geno2prune.py
+++ wgs/geno2prune.py
@@ -6,12 +6,13 @@
 # CHROM POS GENE SNPS HAPLOTYPES FST AVG_FST MAX_FST MIN_FST POP1 POP2 ...
 
 
-from seqpy import cout, cerr
+from seqpy import cout, cerr, cexit
 from seqpy.cmds import arg_parser
 from seqpy.core.bioio import tabparser
 
 import allel
 import numpy as np
+import itertools
 
 from seqpy.core.funcs import pruner
 
@@ -19,6 +20,8 @@ def init_argparser(p=None):
 
     p = tabparser.init_argparser()
     p.add_argument('-s', '--scheme', type=int, default=2)
+    p.add_argument('--threshold', type=float, default=0.25)
+    p.add_argument('--region', default='chrom')
     p.add_argument('-o', '--outfile', default='outfile.pruned.txt')
 
     return p
@@ -36,7 +39,31 @@ def geno2prune( args ):
 
     cerr('I: start parsing genotype file...')
 
-    for region in lineparser.parse_chromosomes():
-        cerr('I: pruning %s' % region.name)
+    outfile = open(args.outfile, 'w')
+    outfile.write(lineparser.get_position_header())
+    outfile.write('\n')
+
+    iter_func = {
+        'chrom': lineparser.parse_chromosomes,
+        'whole': lineparser.parse_whole,
+        'gene': lineparser.parse_genes
+    }[args.region]
+
+    for region in iter_func():
+        cerr('I: generating genotype array for %s' % region.name)
         genoarray = allel.GenotypeArray( region.genotypes() )
-        index = pruner.prune_2(genoarray, region.positions())
+
+        cerr('I: pruning for %s' % region.name)
+        if args.scheme == 1:
+            index = pruner.prune_1(genoarray, args.threshold)
+        elif args.scheme == 2:
+            index = pruner.prune_2(genoarray, region.positions(), args.threshold)
+        elif args.scheme == 3:
+            index = pruner.prune_3(genoarray, region.positions(), args.threshold)
+        else:
+            cexit('E: scheme type undefined!')
+
+        new_positions = itertools.compress(region.positions(), index)
+
+        for pos in new_positions:
+            outfile.write('%s\n' % '\t'.join( pos ))
