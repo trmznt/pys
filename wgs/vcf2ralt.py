ab0422df395e0405aaa5ee605c3cd0e8d7d4b573
Hidayat Trimarsanto <anto@eijkman.go.id>
2018-10-25 16:21:18 +0700

    add vcf2ralt.py

---
+++ wgs/vcf2ralt.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env spcli
+
+# this script convert VCF file to 1) ralt (ratio of alternate allele) matrix and 2) SNP/position manifest
+# please note that this script needs to be run under spcli (seqpy)
+
+from seqpy import cout, cerr, cexit
+from seqpy.cmds import arg_parser
+
+try:
+    import allel
+except:
+    cerr('ERR: require properly installed scikit-allel!')
+import numpy as np
+
+def init_argparser():
+    p = arg_parser("Convert VCF to ratio of alternate ref dataset")
+    p.add_argument('-o', '--outfile', default='outdata')
+    p.add_argument('infile')
+
+    return p
+
+def main(args):
+    vcf2ralt( args )
+
+
+def vcf2ralt( args ):
+
+    # read the mighty VCF file
+    cerr('[I: reading VCF...]')
+    vcfset = allel.read_vcf(args.infile,
+            fields=['samples', 'variants/CHROM', 'variants/POS', 'variants/REF',
+                'variants/ALT', 'variants/SNPEFF_GENE_NAME',
+                'variants/SNPEFF_AMINO_ACID_CHANGE', 'calldata/AD'])
+    cerr('[I: read %s site, %s samples]' % (len(vcfset['variants/CHROM']),
+         len(vcfset['samples'])))
+
+    pos_file = args.outfile + '.pos.txt'
+    geno_file = args.outfile + '.ralt.txt'
+
+    # write position
+    # position file is:
+    # CHROM POS REF ALT
+    cerr('[I: writing position file]')
+    with open(pos_file, 'w') as outfile:
+        outfile.write('CHROM\tPOS\tREF\tALT\tGENE\tAACHANGE\n')
+        for (chrom, pos, ref, alt, gene, aachange) in zip(
+                vcfset['variants/CHROM'], vcfset['variants/POS'],
+                vcfset['variants/REF'], vcfset['variants/ALT'],
+                vcfset['variants/SNPEFF_GENE_NAME'],
+                vcfset['variants/SNPEFF_AMINO_ACID_CHANGE']):
+            outfile.write('%s\t%d\t%s\t%s\t%s\t%s\n' %
+                (chrom, pos, ref, alt[0], gene, aachange))
+
+    # write genotype by converting the genotype
+    cerr('[I: writing genotype file]')
+    with open(geno_file, 'w') as outfile:
+        outfile.write( '\t'.join( list( vcfset['samples'])))
+        outfile.write('\n')
+        c = 0
+        for gts in vcfset['calldata/AD']:
+            #np.savetxt( outfile, majgeno(gts), fmt="%d", delimiter="\t" )
+            outfile.write( '\t'.join( '%4.3f' % x for x in ralt(gts)) )
+            outfile.write('\n')
+            c += 1
+            if c % 100 == 0:
+                cerr('[I: writing site %d' % c)
+
+
+def ralt(genotypes):
+
+    data = np.zeros( shape = len(genotypes), dtype=float )
+    for idx, gt in enumerate(genotypes):
+        gt_tot = gt[0] + gt[1]
+        if gt_tot == 0:
+            data[idx] = -1.0
+            continue
+        data[idx] = gt[0]/gt_tot
+
+    return data
