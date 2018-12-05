c9029cda313366736d4f41b9e236f5cc3bd41a5f
Anto - alrescha <anto@eijkman.go.id>
2018-12-05 14:18:25 +0700

    add loop over k and samples

--- wgs/nalt2lktest.py
+++ wgs/nalt2lktest.py
@@ -1,27 +1,164 @@
+# ralt2nalt.py
+#
+# convert ralt file to nalt file
 
+
+from seqpy import cout, cerr, cexit
+from seqpy.cmds import arg_parser
+
+import numpy as np
+
+from seqpy.core.cfuncs import genoutils
+from seqpy.core.bioio import naltparser
+
+import random
+
+def init_argparser():
+    p = arg_parser("Evaluate a set of SNPs for its likelihood-based assignment")
+    p.add_argument('-k', default='1000,500,100', help='k list')
+
+    p = naltparser.init_argparser(p)
+
+    return p
+
+def main(args):
+    nalt2lktest( args )
+
+
+def nalt2lktest( args ):
+
+    nalt_parser = naltparser.NAltLineParser( args, datatype='nalt')
+
+    # we need group info
+    nalt_parser.parse_grouping()
+
+    region = nalt_parser.parse_whole()
+
+    overall_accuracies = []
+    for k in [ int(x) for x in args.k.split(',')]:
+
+        cerr('[I: random sites for k=%d]' % k)
+        accuracies = []
+        for i in range(100):
+            rand_reg = random_sites(region, k)
+            tester = LikelihoodTester(rand_reg, nalt_parser.samples, nalt_parser.group_parser)
+            tester.prepare_matrix()
+            accuracies.append(tester.evaluate())
+            cerr('[I: iteration of %d]' % i)
+
+        overall_accuracies.append( k, accuracies)
+
+    import IPython; IPython.embed()
 
 
 class LikelihoodTester(object):
 
-	def __init__(self, region, groups):
-		self.region = region
-		self.groups = groups
-		self.matrices = None
+    def __init__(self, region, samples, groups):
+        self.region = region
+        self.groups = groups
+        self.samples = samples
+        self.matrices = None
 
 
-	def estimate(self, n_alt_data):
+    def estimate(self, n_alt_data):
 
-		lks = self.calculate_likelihood(n_alt_data)
-		pass
+        lks = self.calculate_likelihoods(n_alt_data)
+        lks.sort()
+        return lks[-1][1]
 
-	def evaluate(self):
-		""" return the accuracy of the region """
-		pass
+    def evaluate(self):
+        """ return the accuracy of the region """
 
-	def prepare_matrix(self):
-		""" create a set of matrices for each group """
-		pass
+        l = len(self.region.M)
+        match = mismatch = 0
+        for i in range(len(self.samples)):
+            # create sample array
+            n_alt_data = np.zero(l)
+            for x in range(l):
+                n_alt_data[x] = self.region.M[x][i]
 
-	def calculate_likelihood(self, n_alt_data):
-		""" calculate likelihood of each group """
-		pass
+            est_group = self.estimate(n_alt_data)
+
+            if est_group == self.groups.group_info[self.samples[i]]:
+                match += 1
+            else:
+                mismatch += 1
+
+        return (match / (match+mismatch))
+
+
+    def prepare_matrix(self):
+        """ create a set of matrices for each group """
+
+        # create empty matrices
+        M = {}
+        l = len(self.region.M)
+        for g in self.groups.groups:
+            M[g] = np.full(shape=(l,2), fill_value=0.01)
+
+        # filling-up matrices
+        # we are iterating column wise
+        for i in range(len(self.samples)):
+            sample = self.samples[i]
+            group = self.groups.group_info[sample]
+
+            for x in range(l):
+                n_alt = self.region.M[x][i]
+                M_grp = M[group]
+                if n_alt == 0:
+                    M_grp[x,0] += 2
+                elif n_alt == 2:
+                    M_grp[x,1] += 2
+                elif n_alt == 1:
+                    M_grp[x,0] += 1
+                    M_grp[x,1] += 1
+
+        # convert M into fractions
+        for g in M:
+            M_grp = M[g]
+            for x in range(l):
+                _0 = M_grp[x,0]
+                _1 = M_grp[x,1]
+                total = _0 + _1
+                M_grp[x,0] = _0 / total
+                M_grp[x,1] = _1 / total
+            M[g] = np.log(M_grp)
+
+        self.matrices = M
+        return M
+
+
+    def calculate_likelihoods(self, n_alt_data):
+        """ calculate log likelihood of each group """
+
+        R = []
+        for g in self.matrices:
+            R.append(
+                ( self.calculate_likelihood(self.matrices[g], n_alt_data), g)
+            )
+
+        return R
+
+
+    def calculate_likelihood(self, M, n_alt_data):
+
+        log_lk = 0.0
+        for x in range(len(n_alt_data)):
+            n_alt = n_alt_data[x]
+            if n_alt == 0:
+                log_lk += 2*M[x,0]
+            elif n_alt == 2:
+                log_lk += 2*M[x,1]
+            elif n_alt == 1:
+                log_lk += (M[x,0] + M[x,1])
+
+        return log_lk
+
+
+def random_sites(region, L):
+    """ creates a new region with randomly selected positions """
+
+    new_region = naltparser.Region('random')
+    for i in range(L):
+        new_region.M.append( random.choice(region.M))
+    return new_region
