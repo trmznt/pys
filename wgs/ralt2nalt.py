d5c613c6ba3e6539ec7798d896823fc6175b8058
Hidayat Trimarsanto <anto@eijkman.go.id>
2018-12-05 09:35:37 +0700

    add real converting functionality

--- wgs/ralt2nalt.py
+++ wgs/ralt2nalt.py
@@ -15,11 +15,11 @@ def init_argparser():
     p = arg_parser("Convert r_alt datafile to n_alt datafile")
     p.add_argument('-o', '--outfile', default='outdata')
     p.add_argument('-r', '--hetratio', type=float, default=0.25,
-    				help = "ratio for calling heterozygosity")
+                    help = "ratio for calling heterozygosity")
     p.add_argument('-m', '--major', action = "store_true",
-    				help = "set all caling to major allele")
+                    help = "set all caling to major allele")
     p.add_argument('--missingtohet', action = 'store_true',
-    				help = "set missing as heterozygosity")
+                    help = "set missing as heterozygosity")
     # option to treat all missing as hets
 
     p = naltparser.init_argparser(p)
@@ -32,12 +32,19 @@ def main(args):
 
 def ralt2nalt( args ):
 
-	ralt_parser = naltparser.NAltLineParser( args, datatype='ralt')
+    ralt_parser = naltparser.NAltLineParser( args, datatype='ralt')
 
-	region = ralt_parser.parse_whole()
+    region = ralt_parser.parse_whole()
 
-	# convert to n_alt
-	region.ralt_to_nalt(hetratio = 0.25)
+    # convert to n_alt
+    region.ralt_to_nalt(hetratio = args.hetratio if not args.major else -1)
 
-	import IPython
-	IPython.embed()
+    # write to outfile
+    with open(args.outfile, 'w') as outfile:
+        # write header
+        outfile.write(ralt_parser.sample_header)
+        outfile.write('\n')
+        np.savetxt(outfile, region.M, fmt ='%d', delimiter = '\t')
+
+    cerr('[I: finish writing to %s' % args.outfile)
+
