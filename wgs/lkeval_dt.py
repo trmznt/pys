dfd64093945da748665f3e4530b377adec69dee4
Anto - alrescha <anto@eijkman.go.id>
2018-12-18 14:12:08 +0700

    add k to decisiontree algo

--- wgs/lkeval_dt.py
+++ wgs/lkeval_dt.py
@@ -4,6 +4,7 @@
 import itertools
 
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
@@ -15,13 +16,14 @@ from seqpy.core.cfuncs import lk_eval
 
 def init_argparser():
     parser = arg_parser("Evaluate a set of SNPs for its likelihood-based assignment")
+    parser.add_argument('-k', default='100,50', help='k list')
     parser.add_argument('-o', '--outfile', default='out.accuracy.dt.txt')
     parser = naltparser.init_argparser(parser)
 
     return parser
 
 
-def evaluate_dt(haplotypes, group_keys, n_inner=100, n_outer=1):
+def evaluate_dt(haplotypes, group_keys, k, n_inner=100, n_outer=1):
     """ perform iteration to do evaluation """
     estimation = []
     c = 0
@@ -32,7 +34,7 @@ def evaluate_dt(haplotypes, group_keys, n_inner=100, n_outer=1):
         for _ in range(n_inner):
             c += 1
 
-            classifier = DecisionTreeClassifier(class_weight='balanced')
+            classifier = DecisionTreeClassifier(class_weight='balanced', max_features=k)
             classifier = classifier.fit(X_train, y_train)
             features = classifier.tree_.feature
 
@@ -74,9 +76,16 @@ def lkeval_dt(args):
 
     region = nalt_parser.parse_whole(mask=mask)
     haplotypes = region.haplotypes()
-    result = evaluate_dt(haplotypes, group_keys, n_inner=100, n_outer=1)
 
-    lk_eval.calculate_accuracy(result, args.outfile)
+    accuracies = []
+    for k in [ int(x) for x in args.k.split(',') ]:
+
+        cerr('[I - iterating for k = %d]' % k)
+        result = evaluate_dt( haplotypes, group_keys, k=k, n_inner=10, n_outer=10)
+        accuracies.append( lk_eval.calculate_accuracy(result, k) )
+
+    result = pd.concat( accuracies )
+    result.to_csv(args.outfile, sep='\t', index=False)
 
 
 def main(args):
