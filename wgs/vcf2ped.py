996ed99a12e51e52522caf5669c568f0e2bbf82b
Hidayat Trimarsanto <anto@eijkman.go.id>
2020-11-18 09:58:33 +0700

    add vcf2ped.py

---
+++ wgs/vcf2ped.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env spcli
+
+from seqpy import cout, cerr, cexit
+from seqpy.cmds import arg_parser
+from seqpy.core.bioio import grpparser
+
+import time
+
+try:
+    import allel
+except:
+    cexit('ERR: require properly installed scikit-allel!')
+
+def init_argparser():
+    p = arg_parser("Create a ped and map file suitable for isoRelate from VCF and metafile")
+    p = grpparser.init_argparser( p )
+    p.add_argument('-o', '--outprefix', default='outdata')
+    p.add_argument('infile')
+    return p
+
+def vcf2ped( args ):
+    """ create a ped and map file based on vcf and metafile, suitable for isoRelate """
+
+    # open group file
+    group_parser = grpparser.GroupParser( args )
+
+    # open VCF file
+    cerr('[I: reading VCF...]')
+    start_time = time.monotonic()
+    vcfset = allel.read_vcf(args.infile,
+                fields = ['samples', 'variants/CHROM', 'variants/POS', 'calldata/GT'])
+    cerr('[I: read %s site, %s samples in %d secs]' % (len(vcfset['variants/CHROM']),
+         len(vcfset['samples']), time.monotonic() - start_time))
+
+    # assign groups
+    samples = vcfset['samples']
+    group_parser.assign_groups(samples)
+    groups = group_parser.group_keys
+    #import IPython; IPython.embed()
+
+    # write to PED
+    with open(args.outprefix + '.ped', 'w') as outf:
+        for i in range(len(samples)):
+            outf.write('%s\t%s\t' % (groups[i], samples[i]))
+            alleles = []
+            for gt in vcfset['calldata/GT'][:,i]:
+                allele_1, allele_2 = gt
+                #print(allele_1, allele_2)
+                if allele_1 == allele_2:
+                    if allele_1 == -1:
+                        alleles += [0, 0]
+                    elif allele_1 == 0:
+                        alleles += [1, 1]
+                    elif allele_1 == 1:
+                        alleles += [2, 2]
+                    else:
+                        alleles += [1, 1]
+                else:
+                    alleles += [1, 2]
+            outf.write('\t'.join( str(i) for i in alleles))
+            outf.write('\n')
+            #import IPython; IPython.embed()
+
+
+def main( args ):
+    vcf2ped( args )
