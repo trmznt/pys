3aea4a1a5da861c947125e402eb86f0927c0ab24
anto @ aleph <trimarsanto@gmail.com>
2022-06-26 21:33:42 +0700

    add saving output

--- wgs/zarr2barcode.py
+++ wgs/zarr2barcode.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env spcli
 
 import argparse
-import numpy as np
+import os
 
+from seqpy import cerr
 from seqpy.core.sgk import sgio, sgutils
-from seqpy.core.bioio import posutils
+from seqpy.core.bioio import posutils, tabutils
 
 
 def init_argparser():
@@ -14,6 +15,7 @@ def init_argparser():
     p.add_argument('-o', '--outfile')
     p.add_argument('--outtarget', default='')
     p.add_argument('--useGT', default=False, action='store_true')
+    p.add_argument('--mindepth', default=5, type=int)
     p.add_argument('--hetratio', default=0.67, type=float,
                    help='The ratio of allele depth over total depth to call hets. '
                    'Value of 0.67 means if depth_of_major_allele/total_depth is < 0.67, '
@@ -42,20 +44,34 @@ def zarr2barcode(args):
 
     # convert using hetratio
 
-    variants = []
-    for var_idx in ds.variants:
+    cerr('[Converting to barcode...]')
+    variants = sgutils.get_alleles(
+        sgutils._allele_for_barcode,
+        ds,
+        hetratio=args.hetratio,
+        mindepth=args.mindepth,
+        useGT=args.useGT
+    )
 
-        variants.append(
-            sgutils.get_alleles(
-                sgutils._allele_for_barcode,
-                ds,
-                hetratio=args.hetratio,
-                mindepth=args.mindepth,
-                useGT=args.useGT
-            )
-        )
+    df_barcode = tabutils.dataframe_from_variants(ds, variants)
 
-    # saving to outfile
+    # saving to ouftile
+    match ext := os.path.splitext(args.outfile)[1].lower():
+
+        case '.tsv' | '.csv':
+            df_barcode.to_csv(args.outfile, sep='\t' if ext == '.tsv' else ',', index=False)
+
+        case '.txt':
+            with open(args.outfile, 'w') as fout:
+                fout.write('#BAR\tSAMPLE\n')
+                for idx, row in df_barcode.iterrows():
+                    fout.write(''.join(row[1:]) + '\t' + row[0] + '\n')
+
+        case _:
+            raise ValueError('other extension is not implemented yet')
+
+    N, L = df_barcode.shape
+    cerr(f'[Barcode (L={L-1};N={N}) is written to {args.outfile}]')
 
 
 def main(args):
