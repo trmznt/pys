c5100738e7bb92567e54985123a2630541681e7d
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2023-10-12 13:56:10 +0930

    add more functionality

--- wgs/tab2anno.py
+++ wgs/tab2anno.py
@@ -2,25 +2,40 @@
 
 from seqpy import cout, cerr, cexit, gzopen
 from seqpy.cmds import arg_parser
-from seqpy.core.bioio import metautils, tabutils
+from seqpy.core.bioio import tabutils
 import sys
 
 
 def init_argparser():
     p = arg_parser("Create colour annotation based on header of a file")
     p.add_argument('--metafile', required=True,
-                   help='TSV/CSV file containing SAMPLES and group columns')
-    p.add_argument('--specfile')
+                   help='TSV/CSV file containing SAMPLES and group columns, '
+                   'with SAMPLES and group columns stated after filename, eg: '
+                   'a_filename.tsv:SAMPLE,Country')
+    p.add_argument('--specfile', default=None,
+                   help='TSV/CSV file containing the group column and other columns')
     p.add_argument('-s', default=False, action='store_true',
                    help='include sample size of each group in legend')
-    p.add_argument('-o', '--outfile', default="outfile.anno")
+    p.add_argument('-o', '--outprefix', default="")
+    p.add_argument('--outsample', default="")
+    p.add_argument('--outgroup', default="")
 
-    p.add_argument('infile')
+    p.add_argument('infile',
+                   help='either a tab-delimited file with a SAMPLE header, or '
+                   'a tab-delimited file with samples as its header, such as '
+                   'a distance matrix file.')
 
     return p
 
 
 def tab2anno(args):
+
+    if not (args.outprefix or args.outsample or args.outgroup):
+        cexit('Please provide at least --outprefix or --outsample or --outgroup')
+
+    if args.outprefix:
+        args.outsample = args.outprefix + '.indv.tsv'
+        args.outgroup = args.outprefix + '.group.tsv'
 
     # read infile using tabutils
     df = tabutils.read_file(args.infile)
@@ -42,27 +57,52 @@ def tab2anno(args):
         columns = columns.split(',')
 
     joined_df = meta_df.meta.join_to_samples(samples, columns)
+
     if len(joined_df) != len(samples):
         diff = set(samples) - set(joined_df['SAMPLE'])
         cerr('The following samples do not have metadata:')
         cerr(f'{diff}')
         sys.exit(1)
 
-    # get spec file and its specs
-    specfile, column = args.specfile.split(':')
-    if not column:
-        raise ValueError(f'Please provide the column key at the end of filename, eg: '
-                         f'{specfile}:COLUMN_NAME')
-    spec_df = tabutils.read_file(specfile)
+    if args.specfile:
+        # get spec file and its specs
+        specfile, column = args.specfile.split(':')
+        if not column:
+            raise ValueError(f'Please provide the column key at the end of filename, eg: '
+                             f'{specfile}:COLUMN_NAME')
+        spec_df = tabutils.read_file(specfile)
+    else:
+        # generate spec_df automatically
+        column = columns[1]
+        spec_df = tabutils.generate_spec_df(joined_df[column].unique(), column)
+
     joined_df = joined_df.meta.join(spec_df, column)
+
     if len(joined_df) != len(samples):
         diff = set(samples) - set(joined_df['SAMPLE'])
         cerr('The following samples do not have metadata:')
         cerr(f'{diff}')
         sys.exit(1)
 
-    tabutils.write_file(args.outfile, joined_df)
-    cerr(f'Output written to {args.outfile}')
+    if args.outsample:
+        tabutils.write_file(args.outsample, joined_df)
+        cerr(f'Sample output written to {args.outsample}')
+
+    if args.outgroup:
+        # need to change 1st column label
+
+        if args.s:
+            sizes = joined_df[column].value_counts()
+
+            for idx, r in spec_df.iterrows():
+                spec = r[column]
+                r[column] = f'{spec} ({sizes[spec]})'
+
+        spec_df.sort_values(by=column, inplace=True)
+        spec_df.rename(columns={column: 'GROUP'}, inplace=True)
+
+        tabutils.write_file(args.outgroup, spec_df)
+        cerr(f'Group output written to {args.outgroup}')
 
 
 def main(args):
