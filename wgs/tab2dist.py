f7f5e6e63e38d87cae943ea8c8371fd443e36102
Anto @ ada.cdu.edu.au <hidayat.trimarsanto@menzies.edu.au>
2023-10-12 13:55:28 +0930

    fix wrong range counting

--- wgs/tab2dist.py
+++ wgs/tab2dist.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from seqpy import cerr
 from seqpy.core.bioio import tabutils
-from numba import njit, prange, int_, float32
+from numba import njit, prange, int_, float32, set_num_threads
 from numba_progress import ProgressBar
 
 
@@ -15,15 +15,19 @@ def init_argparser():
     p = argparse.ArgumentParser()
 
     p.add_argument('-o', '--outfile')
+    p.add_argument('-j', type=int, default=1,
+                   help='Number of threads to be used, default = 1')
     p.add_argument('infile')
     return p
 
 
-@njit(nogil=True, parallel=True)
+#@njit(nogil=True, parallel=True)
 def njit_loop_calc_dist(allele_m, dist_m, n, allele_length, progress_proxy):
     for i in prange(n):
         r_i = allele_m[i]
-        for j in range(i - 1):
+        counter = 0
+        for j in range(i):
+            counter += 1
             d = 0
             c = 0
             r_j = allele_m[j]
@@ -36,9 +40,9 @@ def njit_loop_calc_dist(allele_m, dist_m, n, allele_length, progress_proxy):
                 if x_k != y_k:
                     d += 1
             dist_m[i, j] = dist_m[j, i] = (d / c)
-            if (j + 1) % 200 == 0:
+            if counter % 200 == 0:
                 progress_proxy.update(200)
-        progress_proxy.update(j % 200)
+        progress_proxy.update(counter % 200)
 
 
 def calc_dist(allele_df):
@@ -56,6 +60,8 @@ def calc_dist(allele_df):
 
 
 def tab2dist(args):
+
+    set_num_threads(args.j)
 
     cerr(f'Reading genomic data from {args.infile}')
     df = tabutils.read_file(args.infile)
